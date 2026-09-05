55c5584012bebef7892612ce1665ecc6ad8053d1
Satish Yadav <satishyadav963172@gmail.com>
2026-09-05 09:24:01 +0530

    Time: 2 ms (24.44%), Space: 9.3 MB (57.55%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include <stdbool.h>
+    bool isPalindrome(char* s) {
+        int left = 0;
+            int right = 0;
+     while (s[right] != '\0') {
+        right++;
+     }
+ right--;
+ while (left < right) {
+while (left < right &&!isalnum(s[left])){
+left++;
+}                                      while (left < right && !isalnum(s[right])) {                       right--;
+}                                      if (tolower(s[left]) != tolower(s[right])) {                       return false;
+                                                    
+                                                                                  }
+
+                                                                                                                                left++;
+                                                                                                                                        right--;
+ }                                             return true;
+    }
