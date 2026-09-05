8626558c3a2992c5d8dafaed2f19cc40c0ac0429
Satish Yadav <satishyadav963172@gmail.com>
2026-09-05 09:34:19 +0530

    Time: 0 ms (100%), Space: 8.4 MB (63.49%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.c
@@ -0,0 +1,9 @@
+bool isPerfectSquare(int num) {
+    long long i;
+    for (i = 1; i * i <= num; i++) {
+        if (i * i == num) {
+            return true;
+        }
+    }
+    return false;
+}
