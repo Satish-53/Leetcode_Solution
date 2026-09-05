f42a1c495ff207bb312ba1c9c50ce76dbf29d999
Satish Yadav <satishyadav963172@gmail.com>
2026-09-05 09:36:56 +0530

    Time: 3 ms (24.72%), Space: 10.9 MB (89.15%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.c
@@ -0,0 +1,24 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** fizzBuzz(int n, int* returnSize){
+    char** answer=malloc(n*sizeof(char*));
+    *returnSize=n;
+    for (int i=1;i<= n;i++){
+        if (i % 15==0){
+            answer[i-1]= "FizzBuzz";
+        }
+        else if (i%3==0){
+            answer[i-1] = "Fizz";
+        }
+        else if (i%5==0) {
+            answer[i-1] = "Buzz";
+        }
+        else{
+         char* number = malloc(12*sizeof(char));
+            sprintf(number,"%d",i);
+            answer[i-1]=number;
+        }
+    }
+return answer;
+}
