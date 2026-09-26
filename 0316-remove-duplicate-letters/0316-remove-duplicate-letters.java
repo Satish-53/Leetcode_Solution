0e3018ce22dbfc30355ecf19aa13d27d3775fae8
Satish Yadav <satishyadav963172@gmail.com>
2026-09-26 06:55:00 +0530

    Time: 2 ms (79.49%), Space: 43.2 MB (97.2%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,23 +1,23 @@
 class Solution {
-    public String removeDuplicateLetters(String s) {
+    public String removeDuplicateLetters(String s){
         int[] count=new int[26];
         // count each letter how much time its repeat
-        for (int i=0;i<s.length();i++){
+        for(int i=0;i<s.length();i++){
             count[s.charAt(i)-'a']++;
         }
         boolean[] used=new boolean[26];
         StringBuilder answer=new StringBuilder();
-        for (int i=0;i<s.length();i++){
+        for(int i=0;i<s.length();i++){
             char current=s.charAt(i);
             int currentIndex=current-'a';
             // using a single uccurrence for this letter
             count[currentIndex]--;
             //  if letter is already in answer then skip it
-            if (used[currentIndex]){
+            if(used[currentIndex]){
                 continue;
             }
             //remove capital letter when its repeat after thenn
-            while (answer.length()>0)
+            while(answer.length()>0)
             {
                 char last=answer.charAt(answer.length()-1);
                 int lastIndex=last-'a';
